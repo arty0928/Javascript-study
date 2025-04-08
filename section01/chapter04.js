d9a89e116f66e0b319b936e32b722aee3e94ca5b
arty0928 <arty0928@konkuk.ac.kr>
2025-04-08 17:47:46 +0900

    [1-4] (실습) 변수와 상수

---
+++ section01/chapter04.js
@@ -0,0 +1,29 @@
+// 1. 변수
+let age;
+console.log(age);
+
+age = 30;
+console.log(age);
+
+// 2. 상수
+const birth = "2000.09.28";
+console.log(birth);
+// birth = "123"; 상수는 값 재할당 불가
+
+// 3. 변수 명명규칙 (네이밍 규칙)
+
+// 3-1. $, _ 제외 기호 사용 불가
+let $_name;
+
+// 3-2. 숫자로 시작 X
+let name1;
+let name2;
+
+// 3-3. 예약어 사용 X
+
+
+// 4. 변수 명명 가이드
+let a = 1;
+let b = 1;
+let c = a - b;
+console.log(c);
