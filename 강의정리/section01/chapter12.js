47c9d4c1c39c64eb7163b88786c64b8532ff8309
arty0928 <arty0928@konkuk.ac.kr>
2025-04-15 17:18:28 +0900

    [1-12] 함수 표현식과 화살표 함수

--- 강의정리/section01/chapter12.js
+++ 강의정리/section01/chapter12.js
@@ -4,3 +4,17 @@ function funA() {
 
 let varA = funA;
 varA();
+
+
+// 1. 함수 표현식
+// 함수 표현식은 호이스팅되지 않음
+let varB = function () {
+    console.log("funB");
+}
+
+varB();
+
+// 2. 화살표 함수
+let varC = (value) => value + 1;
+console.log(varC(10));
+
