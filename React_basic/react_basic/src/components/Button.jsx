bd1deb25383cd50ff67a60e1d3280d9d9d1705bf
arty0928 <arty0928@konkuk.ac.kr>
2025-04-30 11:39:20 +0900

    [5-5] 이벤트 처리하기

--- React_basic/react_basic/src/components/Button.jsx
+++ React_basic/react_basic/src/components/Button.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Button = ({children,text, color = "black"}) => {
+    
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(text);
+        console.log(e);
+        // console.log(e.children);
+        console.log(e.target.children);
+    }
+    
     return (
-        <button style={{ color: color }}>{text} - {color.toUpperCase()}
+        <button
+            // onMouseEnter={onClickButton}
+            onClick={onClickButton} style={{ color: color }}>{text} - {color.toUpperCase()}
             {children}
         </button>
     );
