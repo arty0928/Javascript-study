c7c4ae75def7a801d6ffe98000e707622bdfea7f
arty0928 <arty0928@konkuk.ac.kr>
2025-04-30 11:31:07 +0900

    [5-4] Props로 데이터 전달하기

---
+++ React_basic/react_basic/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({children,text, color = "black"}) => {
+    return (
+        <button style={{ color: color }}>{text} - {color.toUpperCase()}
+            {children}
+        </button>
+    );
+};
+
+
+export default Button;
